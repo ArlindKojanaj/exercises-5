2826d46039770e68b92c7e57af646e32a76eb461
Arlind K <kojaniarlind1@gmail.com>
2022-10-13 14:16:43 +0200

    running at localhost 5000

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const http = require("http");
 
 const hostname = "localhost";
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.end("Running at localhost 5000");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
