2004cb52b233f1f864dd59a72a7d91ead76742c0
Arlind K <kojaniarlind1@gmail.com>
2022-10-14 13:22:28 +0200

    exercise 5 part 2

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const http = require("http");
+var fs = require("fs");
+var index = fs.readFileSync("index.html");
 
 const hostname = "localhost";
 const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Running at localhost 5000");
+  res.setHeader("Content-Type", "text/html");
+  res.end(index);
+
+  // res.end("Running at localhost 5000");
+  console.log(res);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/nacktschnecke`);
 });
